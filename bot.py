e9335a3fef3e356252d074d2198104ba0516b910
Volodymyr Kozlov <vladimir.kozlov@ext.us.panasonic.com>
2026-02-14 13:40:20 +0100

    fixes

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ from handlers import (
     start_cmd, help_cmd, stats_cmd, level_cmd, achievements_cmd,
     set_personality_cmd, dontwritefirst_cmd, writefirst_cmd,
     mood_cmd, clear_mood_cmd, mood_lisa_cmd, disable_cmd, reset_cmd,
-    selfie_cmd, circle_cmd, horoscope_cmd,
+    selfie_cmd, nudes_gen_cmd, circle_cmd, horoscope_cmd,
     handle_message, handle_voice, handle_media, error_handler,
 )
 from games import truth_cmd, guess_cmd, riddle_cmd, quiz_cmd, quiz_callback
@@ -47,6 +47,7 @@ def main():
     application.add_handler(CommandHandler("achievements", achievements_cmd))
     application.add_handler(CommandHandler("selfie", selfie_cmd))
     application.add_handler(CommandHandler("circle", circle_cmd))
+    application.add_handler(CommandHandler("nudes", nudes_gen_cmd))
     application.add_handler(CommandHandler("horoscope", horoscope_cmd))
     application.add_handler(CommandHandler("mood_lisa", mood_lisa_cmd))
 
